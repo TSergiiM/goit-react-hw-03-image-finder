fbb8bd64ce26a05092f694459fd220edab022127
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2022-04-19 13:54:31 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,15 @@
+import { Gallery } from './ImageGallery/ImageGallery';
+import { Loader } from './Loader/Loader';
+import { Modal } from './Modal/Modal';
+import { Searchbar } from './Searchbar/Searchbar';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div>
+      <Searchbar />
+      <Gallery />
+      <Modal />
+      <Loader />
     </div>
   );
 };
