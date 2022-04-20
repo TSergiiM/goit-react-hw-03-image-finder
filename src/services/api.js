f6534f08141c8fb33d14cc17bc9cf0a92454ad47
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2022-04-20 19:54:28 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,15 @@
-import axios from 'axios';
+// import axios from 'axios';
+
+const API_KEY = '25371642-f176f31923c7990a059799838';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export function searchQueryAPI(searchQuery) {
+  return fetch(
+    `${BASE_URL}/?q=${searchQuery}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+  ).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    return Promise.reject(new Error(`bla-bla-bla`));
+  });
+}
