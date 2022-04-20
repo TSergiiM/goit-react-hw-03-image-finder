7096f55f3132dce39949128845abf7e0db4f0bac
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2022-04-20 19:54:31 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,9 +3,7 @@ import { Component } from 'react';
 import styles from './ImageGallery.module.css';
 import { Loader } from 'components/Loader/Loader';
 import { Button } from 'components/Button/Button';
-
-const API_KEY = '25371642-f176f31923c7990a059799838';
-const BASE_URL = 'https://pixabay.com/api/';
+import { searchQueryAPI } from '../../services/api';
 
 export class Gallery extends Component {
   state = {
@@ -16,15 +14,7 @@ export class Gallery extends Component {
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.searchQuery !== this.props.searchQuery) {
       this.setState({ status: 'pending' });
-      fetch(
-        `${BASE_URL}/?q=${this.props.searchQuery}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-      )
-        .then(response => {
-          if (response.ok) {
-            return response.json();
-          }
-          return Promise.reject(new Error(`blablabla`));
-        })
+      searchQueryAPI(this.props.searchQuery)
         .then(searchQuery => this.setState({ searchQuery, status: 'resolved' }))
         .catch(error => this.setState({ error, status: 'rejected' }));
     }
@@ -44,8 +34,8 @@ export class Gallery extends Component {
         <>
           <ul className={styles.ImageGallery}>
             <GalleryItem searchQuery={searchQuery} />
-            <Button>Load more</Button>
           </ul>
+          <Button>Load more</Button>
         </>
       );
     }
