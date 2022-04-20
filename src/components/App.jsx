63dfc4a37bfb72831e801d7c97010b18f94d3452
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2022-04-20 07:45:07 +0300

    Add-app-module.css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,10 @@ import { Gallery } from './ImageGallery/ImageGallery';
 import { Loader } from './Loader/Loader';
 import { Modal } from './Modal/Modal';
 import { Searchbar } from './Searchbar/Searchbar';
-
+import styles from './App.module.css';
 export const App = () => {
   return (
-    <div>
+    <div className={styles.App}>
       <Searchbar />
       <Gallery />
       <Modal />
