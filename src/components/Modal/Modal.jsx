d0e663ee5005761bcc32ce69508401fdd20b82ad
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2022-04-21 01:15:41 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,38 @@
 import styles from './Modal.module.css';
-export const Modal = () => {
-  return (
-    // <div className={styles.Overlay}>
-    <div className={styles.Modal}>
-      <img src="" alt="" />
-    </div>
-    // </div>
-  );
-};
+import { createPortal } from 'react-dom';
+import { Component } from 'react';
+
+const modalRoot = document.querySelector('#modal-root');
+
+export class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = event => {
+    if (event.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  handlebackdropClick = event => {
+    if (event.currentTarget === event.target) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    return createPortal(
+      <div className={styles.Overlay} onClick={this.handlebackdropClick}>
+        <div className={styles.Modal}>
+          <img src="" alt="" width={200} height={200} />
+        </div>
+      </div>,
+      modalRoot
+    );
+  }
+}
