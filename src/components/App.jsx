397a6576e1a78652ae282d55f76d19de1c974d0e
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2022-04-20 19:26:40 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,22 @@
 import { Gallery } from './ImageGallery/ImageGallery';
-import { Loader } from './Loader/Loader';
 import { Modal } from './Modal/Modal';
 import { Searchbar } from './Searchbar/Searchbar';
 import styles from './App.module.css';
-export const App = () => {
-  return (
-    <div className={styles.App}>
-      <Searchbar />
-      <Gallery />
-      <Modal />
-      <Loader />
-    </div>
-  );
-};
+import { Component } from 'react';
+export class App extends Component {
+  state = {
+    searchQuery: '',
+  };
+  handleSearchSubmit = searchQuery => {
+    this.setState({ searchQuery });
+  };
+  render() {
+    return (
+      <div className={styles.App}>
+        <Searchbar onSubmit={this.handleSearchSubmit} />
+        <Gallery searchQuery={this.state.searchQuery} />
+        <Modal />
+      </div>
+    );
+  }
+}
